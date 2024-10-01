6dddbf967b57a8fb6cb16679104b5adf018e97e5
MAVERICK <agentyarikt7@gmail.com>
2024-10-01 19:03:09 +0300

    qwe

---
+++ script.js
@@ -0,0 +1,38 @@
+const keys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z'];
+
+let currentKeyIndex = 0;
+
+const keyElement = document.getElementById('key');
+const newGameBtn = document.getElementById('newGameBtn');
+
+
+function randomKey (){
+    currentKeyIndex = Math.floor(Math.random() * keys.length)
+    keyElement.textContent = keys[currentKeyIndex]
+}
+
+function keyDown (event) {
+    if(event.key ===  keys[currentKeyIndex]){
+        randomKey()
+    }
+    else{
+        PNotify.error({
+            text:`Це не вірно`,
+            delay: 2000
+        })
+    }
+}
+
+
+function keyPress(event){
+    event.preventDefault()
+}
+
+function newGame(){
+    randomKey
+}
+
+window.addEventListener("keydown" , keyDown)
+window.addEventListener("keypress" , keyPress)
+newGameBtn.addEventListener("click" , newGame)
+newGame()
